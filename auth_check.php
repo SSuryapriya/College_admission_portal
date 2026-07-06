a39b75ec01a1b34b3c62c17bb086802a86652438
SSuryapriya <suryapriyas2005@gmail.com>
2026-07-06 09:08:41 +0530

    Delete auth_check.php

--- auth_check.php
+++
@@ -1,7 +0,0 @@
-<?php
-if (session_status() === PHP_SESSION_NONE) session_start();
-if (!isset($_SESSION['user_id'])) {
-    header("Location: login.php"); exit;
-}
-$user_id = (int)$_SESSION['user_id'];
-$user_name = $_SESSION['user_name'] ?? 'User';
